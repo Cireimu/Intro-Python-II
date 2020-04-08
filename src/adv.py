2ee15b5e510f78c997b8e98230d375ad8c614b8d
Cireimu <ethanhoover3@gmail.com>
2020-04-08 14:41:38 -0700

    reached day one mvp

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,7 +39,9 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
-
+player = Player(input("What's your name? "), room["outside"])
+print(f"Welcome, {player.name}")
+print(player.current_room)
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +52,12 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+while True:
+    cmd = input("=> ").lower()
+    if cmd in ["n", "s", "e", "w"]:
+        player.move(cmd)
+    elif cmd == "q":
+        print("Thanks for playing!")
+        quit()
+    else:
+        print("Unknown command")
